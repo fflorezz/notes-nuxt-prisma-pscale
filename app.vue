c6bed21df1bb1c8e4b957160d7c9020e2e65334d
fflorezz <felipeflorezart@gmail.com>
2023-07-25 18:15:26 -0500

    Add tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="p-10">
+    <h1
+      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+    >
+      My awesome notes
+    </h1>
+    <div class="mt-16">
+      <card />
+    </div>
   </div>
 </template>
